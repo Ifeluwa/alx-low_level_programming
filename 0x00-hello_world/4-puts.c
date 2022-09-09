808d28a041920589842a656aa707d93eee1f0151
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-09-09 03:40:06 -0700

    First C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * REturn: Always 0 (Succcess)
+ */
+int main(void)
+{
+	char myStr[] = "Programming is like building a multilingual puzzle.";
+
+	puts(myStr);
+
+	return (0);
+}

804c1a0ae8d1c586d177683e322de8a2684f77a3
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-09-11 21:05:32 -0700

    ALPHABETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int alphabet, ALPHATBET;
+	alphabet = 'a';
+	ALPHABET = 'A';
+	while (alphabet <= 'z');
+		{
+			putchar(alphabet);
+		}
+	while (ALPHABET <= 'Z');
+		{
+			putchar(ALPHABET);
+		}
+	putchar('\n');
+	return(0);
+	
+}

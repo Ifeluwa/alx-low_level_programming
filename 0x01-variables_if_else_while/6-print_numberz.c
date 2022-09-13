e5eafdedec7bd51bdfce03ef3e531130f3e74178
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-09-12 19:41:39 -0700

    Print numberz with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ ** main - This code will print all single digit numbers
+ **
+ ** Return: 0 (Success)
+ **/
+int main(void)
+{
+	int digit;
+
+	digit = '0';
+		while (digit <= '9')
+		{
+			putchar(digit);
+			digit++;
+		}
+	putchar('\n');
+	return (0);
+}

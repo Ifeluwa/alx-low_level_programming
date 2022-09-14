f582db5d83ea6852711e089c6eeb77c17fbe16ca
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-09-13 20:37:06 -0700

    Print putchar all by myself

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point (This proram will print _putchar)
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char str[9] = "_putchar";
+
+	printf("%s", str);
+	putchar('\n');
+
+	return (0);
+
+}

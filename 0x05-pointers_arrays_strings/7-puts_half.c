8dbcec060decb437e3c23a8f4b685856f4f20ba8
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-09-20 21:40:17 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,9 +13,10 @@ void puts_half(char *str)
 
 	for (l = 0; str[l] != '\0'; ++l)
 		;
+
 	if (l % 2 == 0)
 	{
-		for (n = 1 / 2; str[n] != '\0'; ++n)
+		for (n = l / 2; str[n] != '\0'; ++n)
 			_putchar(str[n]);
 	}
 	else

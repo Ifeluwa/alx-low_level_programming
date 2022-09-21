b001e42b72e786a8171b17cab15bc573fa6e0d3a
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-09-20 21:06:49 -0700

    New one

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints strin in reverse
+ * @s: the string
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int index;
+
+	/*finds the length of string without null character*/
+	for (index = 0; s[index] != '\0'; ++index)
+		;
+
+	/*print char from the last index as you decrement*/
+	for (--index; index >= 0; --index)
+	_putchar(s[index]);
+	_putchar('\n');
+}

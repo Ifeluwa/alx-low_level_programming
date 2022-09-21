8d32b40eabfd1730b3916b09ae89ac2ff9c90aa1
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-09-20 17:29:52 -0700

    Defer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - This code will reset int values to 98
+ * @n: a pointer the integer we want to set to 98
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

c6d7d76cec68365fe6ade628e76483a5bb878701
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-09-22 20:32:47 -0700

    To uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdio.c>
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase to upper
+ *
+ * @s: string to be changed
+ *
+ * Return: Upper "S"
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		i++;
+	}
+	return (s);
+}

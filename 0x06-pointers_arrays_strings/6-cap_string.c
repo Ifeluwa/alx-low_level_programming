533c65cb39b5430b4d69a87cc7cfd37bfef87fd2
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-09-22 20:54:37 -0700

    Capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * cap_string - Capitalizes each word in string
+ *
+ * @s: the string to capitalize
+ *
+ * Return: Capitalized string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+
+		if (s[i - 1] == ' ' ||
+		    s[i - 1] == '\t' ||
+		    s[i - 1] == '\n' ||
+		    s[i - 1] == ',' ||
+		    s[i - 1] == ';' ||
+		    s[i - 1] == '(' ||
+		    s[i - 1] == ')' ||
+		    s[i - 1] == '{' ||
+		    s[i - 1] == '}' ||
+		    s[i - 1] == '.' ||
+		    s[i - 1] == '!' ||
+		    s[i - 1] == '?' ||
+		    s[i - 1] == '"' ||
+		    i == 0)
+			s[i] -= 32;
+
+		i++;
+	}
+
+	return (s);
+}

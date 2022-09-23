c502de799f308a606fc407d423c89053962cda88
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-09-22 20:00:13 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy - this copies a string
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: num of char
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}

83f90e5826fa4ad12e5aac0efaefa4895caf302a
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-09-27 12:15:13 -0700

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ *
+ * @haystack: bigger string
+ * @needle: smaller string
+ *
+ * Return: Found string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}

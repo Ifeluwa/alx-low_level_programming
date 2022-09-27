603de8f29104fddadc14c7411cc122b2d9c1511a
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-09-27 11:59:24 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any set of bytes
+ *
+ * @s: string to search
+ * @accept: string of char to be searched
+ *
+ * Return: new string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+
+		s++;
+	}
+
+	return ('\0');
+}

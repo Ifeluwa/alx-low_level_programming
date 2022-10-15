8a510a078734a1473d703cc5440bf6094b74dba1
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-10-15 10:25:31 -0700

    malloc grad

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include <main.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - returns pointer to 2D
+ *
+ * @width: width
+ * @height: height
+ *
+ * Return: pointer to grid or NULL of failure
+ */
+int **alloc_grid(int width, int height)
+{
+	int **twoD;
+	int hgt, wid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	twoD = malloc(sizeof(int *) * height);
+
+	if (twoD == NULL)
+		return (NULL);
+
+	for (hgt = 0; hgt < heigt; hgt++)
+	{
+		twoD[hgt] = malloc(sizeof(int) * width);
+
+		if (twoD[hgt] == NULL)
+		{
+			for (; hgt >= 0; hgt--)
+				free(twoD[hgt]);
+
+			free(twoD);
+			return (NULL);
+		}
+	}
+
+	for (hgt = 0; hgt < height; hgt++)
+	{
+		for (wid = 0; wid < width; wid++)
+			twoD[hgt][wid] = 0;
+	}
+
+	return (twoD);
+}

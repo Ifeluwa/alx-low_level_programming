de2bc2c471316007a4675aaa20d9be8499aed309
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-10-18 21:18:17 -0700

    Structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: information about a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */

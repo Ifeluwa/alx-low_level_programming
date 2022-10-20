563aaa3e04c24f79dd2e20a92668b6d33c3da70a
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-10-20 08:35:23 -0700

    READ ALL THE INSTRUCTIONS WOG!

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,22 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age == NULL)
+		printf("Age: (nil\n)");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+
+	else
+		printf("Owner: %s\n", d->owner);
 }

76554c2a1c7c0ba8a7426d1f160ad0123f8e8cef
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-10-20 08:22:10 -0700

    Print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints a strct dog.
+ *
+ * @d: the strct dog.
+ *
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}

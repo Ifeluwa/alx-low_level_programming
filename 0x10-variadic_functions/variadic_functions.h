37b58cb9ed9832727329aa63e2d27f64c97854bb
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-10-29 06:34:51 -0700

    Task 0

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+/**
+ * struct printer - struct definin a printer
+ * @symbol: A symbol representing a data type
+ * @print: A pointer to a function that prints data type
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
+int sum_them_all(const unsigned int n, ...);

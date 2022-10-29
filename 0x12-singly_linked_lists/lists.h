e9427bf497b52b82168e299eb871be22dc89e0d1
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-10-29 06:57:35 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct list_s - singly linked list
+ * @str: strin - (malloced string)
+ * @len: length of the string
+ * @next: points to next node
+ *
+ * Description: singly linked list ALX
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif

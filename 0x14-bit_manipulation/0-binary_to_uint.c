84d59541a498ef9169e355622c888e71e5c13ba3
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-11-03 21:59:34 -0700

    NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * binary_to_uint - converts binary to int
  *
@@ -11,6 +12,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int res = 0;
 
 	if (!*b)
+		return (0);
+	if (b == NULL)
 		return (0);
 
 	while (*b)

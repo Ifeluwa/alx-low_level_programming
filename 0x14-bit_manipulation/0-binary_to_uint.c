864aa78554b8326cd8b09106b85a2bd90e7f575d
Ifeoluwa Deborah <ifeoluwadeborahadegbola@gmail.com>
2022-11-03 21:51:40 -0700

    Bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary to int
+ *
+ * @b: the binary number
+ *
+ * Return: coverted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res = 0;
+
+	if (!*b)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		res <<= 1;
+		if (*b & 1)
+			res += 1;
+		b++;
+	}
+	return (res);
+}
